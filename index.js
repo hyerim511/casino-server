f8a9774e83b16371981b96860b0d0799f291dec1
hyerim <kimhyerim511@gmail.com>
2023-08-15 20:14:44 -0700

    add login module

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const textBodyParser = bodyParser.text({
 });
 
 // Modules
+const { authenticateUser } = require('./my_modules/login.js');
 
 // CORS
 app.use(cors({
@@ -32,11 +33,60 @@ app.options('/login', (req,res) => {
 });
 
 
-// GET
-// app.get('/', textBodyParser, async function(req, res){});
+// GET for Login
+app.get('/login',textBodyParser, async function(req, res){
+    // Print the HTTP request headers
+    console.log('req.headers: ', req.headers);
 
-// POST
-// app.post('/', textBodyParser, async function(req, res){});
+    const reqOrigin = req.headers['origin'];
+    const reqTask = req.headers['task'];
+
+    console.log("Request from" + reqOrigin + "for route" + req.url + "with method " + req.method + "for task" + reqTask);
+
+    // TASK Check
+    if (reqTask === 'login') {
+        try {
+            const loginResult = await authenticateUser(req);
+            console.log('authenticateUser result: ', loginResult);
+            if(loginResult == true) {
+                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Access-Control-Expose-Headers', 'request-result');
+                res.setHeader('request-result', 'Request ' + req.method + ' was received successfully');
+                res.status(200).send("Login Successful");
+            } else {
+                res.status(403).send("Login Failed");
+            }
+        } catch(error) {
+            console.log("authenticateUser error: ", error);
+            res.status(500).send("Server Error");
+        }
+    }
+    res.end();
+});
+
+// POST for Signup
+app.post('/login', async function(req, res) {
+    console.log('req.headers: ', req.headers);
+
+    const reqOrigin = req.headers['origin'];
+    const reqTask = req.headers['task'];
+    const reqBody = req.body;
+
+    console.log("Request from" + reqOrigin + "for route" + req.url + "with method " + req.method + "for task" + reqTask);
+    console.log(reqBody);
+
+    if (reqTask === 'signup') {
+        try {
+            const filePath = './data/users.json';
+            const username = reqBody.username;
+            const password = reqBody.password;
+            await addUser(filePath, username, password);
+        } catch(error) {
+            console.log("error: ", error);
+            res.status(500).send("Server Error");
+        }
+    }
+});
 
 
 // Listen
